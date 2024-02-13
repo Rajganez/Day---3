70d28b95b05e3cbf73ca14b47dd710c185a1912a
Rajganez <ganezsouth@live.com>
2024-02-13 13:52:28 +0530

    palidrome

---
+++ Palindrome.js
@@ -0,0 +1,54 @@
+//Return all the palindromes in an array
+
+let arr = ["madam","civic","guvi","india","radar"];
+let palindrome = function(arr){
+let str = "";
+let str1 = "";
+    for (let i in arr){
+       str = arr[i];
+        for(let j = str.length-1 ; j >= 0 ; j-- ){
+            str1 += str[j];    
+            }
+            if(str === str1){
+                console.log(str);
+                str1 = "";
+            }else{
+                str1 = "";
+                        }
+        }
+}
+palindrome(arr); */
+
+Output: 
+madam
+civic
+radar
+
+
+//------------------------------------------
+
+//Using IIFE functions
+
+let arr = ["madam","civic","guvi","india","radar"];
+((arr) => {
+let str = "";
+let str1 = "";
+    for (let i in arr){
+       str = arr[i];
+        for(let j = str.length-1 ; j >= 0 ; j-- ){
+            str1 += str[j];    
+            }
+            if(str === str1){
+                console.log(str);
+                str1 = "";
+            }else{
+                str1 = "";
+                        }
+        }
+})(arr);
+
+Output: 
+madam
+civic
+radar
+
